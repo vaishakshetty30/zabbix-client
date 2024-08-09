5ecb4db198bcc9d68df2b4e29f12d99a40ce00e1
Vaishak Shetty <Vaishak.Shetty@Mediakind.com>
2024-08-09 18:26:03 +0530

    Fix login api

--- base.go
+++ base.go
@@ -132,7 +132,7 @@ func (api *API) CallWithError(method string, params interface{}) (response Respo
 // Calls "user.login" API method and fills api.Auth field.
 // This method modifies API structure and should not be called concurrently with other methods.
 func (api *API) Login(user, password string) (auth string, err error) {
-	params := map[string]string{"user": user, "password": password}
+	params := map[string]string{"username": user, "password": password}
 	response, err := api.CallWithError("user.login", params)
 	if err != nil {
 		return
